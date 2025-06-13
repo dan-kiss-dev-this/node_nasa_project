05e485a6aa32c80005dc31d41c74e5718874ede6
Daniel Kiss <flowfast47@gmail.com>
2025-06-12 17:33:25 -0700

    have index read csv file

--- index.js
+++ index.js
@@ -1,38 +1,23 @@
-const assert = require('node:assert')
 const { parse } = require('csv-parse');
-// import { parse } from 'csv-parse';
-// import assert from "node:asset"
 const fs = require('fs')
 
-// note in the stream example the csv data is sent through the write function and the resulting data is obtained within the readable event by calling the read function
+const results = [];
 
-const records = [];
-// initialize the parser
-const parser = parse({
-    delimiter: ",",
-});
+fs.createReadStream('kepler_data.csv')
+    // pipe() connects readable stream to writeable stream, parse() is destination of pipe.
+    .pipe(parse({
+        comment: '#',
+        // used to return each row in the csv as a javascript object with key value pairs instead of just as an array
+        columns: true,
+    }))
+    .on('data', (data) => {
+        results.push(data)
+    })
+    .on('error', (err) => console.log(err))
+    .on('end', () => {
+        console.log(results)
+    })
 
-//use the readable stream api to consume the records, this is using an event emitter
-parser.on("readable", function () {
-    let record;
-    while ((record = parser.read()) !== null) {
-        console.log(18, record)
-        records.push(record);
-    }
-})
 
-parser.on('error', function (err) {
-    console.error(err.message);
-})
-
-parser.on('end', function () {
-    assert.deepStrictEqual(records, [
-        ['root', 'x'], ['admin', '1']
-    ])
-});
-
-parser.write("root,x\nadmin,1")
-
-parser.end()
 
 
